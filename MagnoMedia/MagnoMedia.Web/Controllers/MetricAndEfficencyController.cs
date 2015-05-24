c556826ae19e95a96922f494bdde5a92edffbf07
Gitzone <amitkumardev89@gmail.com>
2015-05-24 12:04:53 +0530

    Metrics Page UI
    New  metrics page addd

---
+++ MagnoMedia/MagnoMedia.Web/Controllers/MetricAndEfficencyController.cs
@@ -0,0 +1,38 @@
+﻿using MagnoMedia.Data.Models;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MagnoMedia.Web.Controllers
+{
+    public class MetricAndEfficencyController : Controller
+    {
+        //
+        // GET: /MetricAndEfficency/
+
+        public ActionResult Index()
+        {
+            try
+            {
+                IDbConnectionFactory dbFactory = new OrmLiteConnectionFactory(ConfigurationManager.ConnectionStrings["db"].ConnectionString, MySqlDialect.Provider);
+                using (IDbConnection db = dbFactory.Open())
+                {
+                    ViewBag.CountryList = new SelectList(db.Select<Country>(), "Id", "Country_name");
+
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
